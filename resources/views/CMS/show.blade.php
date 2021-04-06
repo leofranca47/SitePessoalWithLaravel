872045cfd92ec0560da4142806202e545bad6e16
Leonardo <leoprofissional@live.com>
2021-04-06 07:15:44 -0300

    inserido margin do bootstrap

--- resources/views/CMS/show.blade.php
+++ resources/views/CMS/show.blade.php
@@ -1,10 +1,13 @@
 @extends('CMS.layout.app')
 
 @section('content')
-    <h2>{{ $post->titulo }}</h2>
-    <a href="{{ route('cms.index') }}" class="btn btn-primary">voltar</a>
+    <h2 class="mb-3">{{ $post->titulo }}</h2>
+    <div class="voltar mb-3">
+        <a href="{{ route('cms.index') }}" class="btn btn-primary">voltar</a>
+    </div>
+
     @if ($post->photo)
-        <img src="{{ Storage::url("{$post->photo}") }}" alt="{{ $post->titulo }}" width="400px">
+        <img src="{{ Storage::url("{$post->photo}") }}" alt="{{ $post->titulo }}" width="400px" class="mb-3">
     @endif
     {!! $post->artigo !!}
 @endsection
