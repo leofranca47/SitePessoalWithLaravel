bfe92b1ef8b59bc3e7026a21ece61b013a2ec97f
Leonardo <leoprofissional@live.com>
2021-04-12 07:29:35 -0300

    foi feito o css da leitura do blog

--- resources/views/CMS/show.blade.php
+++ resources/views/CMS/show.blade.php
@@ -6,27 +6,28 @@
 
     <main>
         <section class="post">
-            <header>
-                <h1>{{ $post->titulo }}</h1>
-                <p class="icon-star-full"></p>
-            </header>
-            <div class="voltar mb-3">
-                <a href="{{ route('cms.index') }}" class="btn btn-primary">voltar</a>
-            </div>
+            <div class="content">
+                <header>
+                    <h1>{{ $post->titulo }}</h1>
+                </header>
+                <div class="voltar">
+                    <a href="{{ route('cms.index') }}" class="btn btn-primary">voltar</a>
+                </div>
 
-            <h6>Postado em : {{ $post->created_at->format('d/m/Y') }}</h6>
+                <h6>Postado em : {{ $post->created_at->format('d/m/Y') }}</h6>
 
 
-            @if ($post->photo)
-                <img src="{{ url("/storage/{$post->photo}") }}" alt="{{ $post->titulo }}" width="400px" class="mb-3">
-            @endif
-            {!! $post->artigo !!}
+                @if ($post->photo)
+                    <img src="{{ url("/storage/{$post->photo}") }}" alt="{{ $post->titulo }}" width="400px"
+                        class="mb-3">
+                @endif
+                {!! $post->artigo !!}
 
-            <form action="{{ route('cms.delete', $post->id) }}" method="POST" enctype="multipart/form-data">
-                @csrf
-                @method('delete')
-                <button type="submit" class="btn btn-danger">Apagar Post</button>
-            </form>
+                <form action="{{ route('cms.delete', $post->id) }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('delete')
+                    <button type="submit" class="btn btn-danger">Apagar Post</button>
+                </form>
             </div>
         </section>
     </main>
